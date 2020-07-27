9ed6c9a7b760843b0f095a1927613611d0e00aca
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-07-27 21:45:16 +0500

    Updates

---
+++ precache-manifest.e07211297f97f20e331d9d2628e09225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a379e8b59b18806096702966217cacff",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "7643ce477fc3dfed9791",
+    "url": "/hangman/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "543cb596fca045fd7c10",
+    "url": "/hangman/static/css/main.0330f001.chunk.css"
+  },
+  {
+    "revision": "7643ce477fc3dfed9791",
+    "url": "/hangman/static/js/2.d6579f11.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/hangman/static/js/2.d6579f11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543cb596fca045fd7c10",
+    "url": "/hangman/static/js/main.f78cc356.chunk.js"
+  },
+  {
+    "revision": "e8e5f2263c89dea7c028",
+    "url": "/hangman/static/js/runtime-main.395feef2.js"
+  }
+]);
