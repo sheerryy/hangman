91b4a0f9ac2f8a1a14d61c9d4b79e70ca3b0d27c
Shehryar <14bscssshoukat@seecs.edu.pk>
2020-07-28 21:48:56 +0500

    add SERVER_PREFIX ('/hangman') for all urls

--- client/src/components/hangman/title_screen/title-screen.tsx
+++ client/src/components/hangman/title_screen/title-screen.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import {Button, Grid, withStyles} from "@material-ui/core";
-import {TitleScreenStyle} from "./title-screen.style";
 import {Link} from "react-router-dom";
+
+import {TitleScreenStyle} from "./title-screen.style";
+import {getServerPrefix} from "../../../utils";
+
+const SERVER_PREFIX = getServerPrefix();
 
 interface PropTypes {
     classes: {
@@ -32,22 +36,22 @@ function TitleScreen({classes}: PropTypes) {
             <Grid item xs={12} className={classes.difficultyArea}>
                 <Grid container spacing={1} justify="center">
                     <Grid item sm={7} xs={10}>
-                        <Link style={{textDecoration: 'none'}} to={'/game/easy'}>
+                        <Link style={{textDecoration: 'none'}} to={`${SERVER_PREFIX}/game/easy`}>
                             <Button size={'large'} variant="outlined" className={classes.difficultyButton}>Easy</Button>
                         </Link>
                     </Grid>
                     <Grid item sm={7} xs={10}>
-                        <Link style={{textDecoration: 'none'}} to={'/game/medium'}>
+                        <Link style={{textDecoration: 'none'}} to={`${SERVER_PREFIX}/game/medium`}>
                         <Button size={'large'} variant="outlined" className={classes.difficultyButton}>Medium</Button>
                         </Link>
                     </Grid>
                     <Grid item sm={7} xs={10}>
-                        <Link style={{textDecoration: 'none'}} to={'/game/hard'}>
+                        <Link style={{textDecoration: 'none'}} to={`${SERVER_PREFIX}/game/hard`}>
                         <Button size={'large'} variant="outlined" className={classes.difficultyButton}>Hard</Button>
                         </Link>
                     </Grid>
                     <Grid item sm={7} xs={10}>
-                        <Link style={{textDecoration: 'none'}} to={'/game/almost-impossible'}>
+                        <Link style={{textDecoration: 'none'}} to={`${SERVER_PREFIX}/game/almost-impossible`}>
                         <Button size={'large'} variant="outlined" className={classes.difficultyButton}>Almost
                             Impossible</Button>
                         </Link>
